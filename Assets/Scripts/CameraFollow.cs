7baf9b7cef6dba598ddf43461f8b46cebae97fcd
Johachim <joachimgul@yahoo.dk>
2019-08-20 10:18:27 +0200

    First Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    public Transform target;
+
+    public float speed = 0.1f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, speed * Time.deltaTime);
+        transform.position = smoothPosition;
+    }
+
+}
---
+++ Assets/Scripts/spawnButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnButton : MonoBehaviour
+{
+    public Transform prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onSpawnButtonClick()
+    {
+        //GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent(typeof(Rigidbody);
+        
+        Instantiate(prefab, new Vector3(2, 4, 2), Quaternion.identity);
+        
+    }
+}
